53ee4d767099f9e0c27e2142e5fcd8a7c9d72ddf
Luka <lukaadomu678@gmail.com>
2026-09-14 11:51:05 +0300

    added ku-proj

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>    
+#include <string>
+using namespace std;
+string vardas;
+int saldainiai;
+int dezutes;
+
+int main() {
+    cout << "Saldainiu kiekis: ";
+    cin >> saldainiai;
+    return 0;
+}
