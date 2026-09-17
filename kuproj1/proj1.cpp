ac901520de55b6cd982e4842424243d4c1883eea
Luka <lukaadomu678@gmail.com>
2026-09-18 02:01:37 +0300

    Added first 2 questions

---
+++ kuproj1/proj1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdint>
+#include <string>
+using namespace std;
+
+string vardas;
+uint32_t amzius;
+float kaina;
+char16_t raide;
+
+int main() {
+    vardas = "Ona";
+    amzius = 20;
+    kaina = 1.65f;
+    raide = 'O';
+    return 0;
+}
---
+++ kuproj1/proj2.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <cstdint>
+#include <string>
+using namespace std;
+
+char16_t raide;
+
+int main() {
+    raide = 'B';
+    cout << raide << endl;
+    return 0;
+}
