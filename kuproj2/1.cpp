54934c0d2c081aa30637fadb42b3e87ec97c24b3
Luka <lukaadomu678@gmail.com>
2026-09-21 10:59:49 +0300

    added proj2

---
+++ kuproj2/1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdint>
+#include <string>
+#include <cmath>
+#include <uchar.h>
+using namespace std;
+
+int main(){
+    char8_t marsrutas;
+    uint16_t atstumas;
+
+    cin >> marsrutas;
+
+
+    return 0;
+}
