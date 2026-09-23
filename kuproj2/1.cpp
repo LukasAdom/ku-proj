8f8f181e9c6cbf3b394a72d3692778b7f7beb6ec
Luka <lukaadomu678@gmail.com>
2026-09-23 16:52:39 +0300

    Okay no more funny switches

--- kuproj2/1.cpp
+++ kuproj2/1.cpp
@@ -6,22 +6,21 @@
 using namespace std;
 
 int main(){
-    char8_t marsrutas;
+    char marsrutas;
     uint16_t atstumas;
 
     cin >> marsrutas;
-    uint16_t imarsrutas = marsrutas - '0';
 
-    switch(imarsrutas){
-        case 27: // K
+    switch(marsrutas){
+        case 'K': // K
             atstumas = 100u;
         break;
 
-        case 28: // L
+        case 'L': // L
             atstumas = 300u;
         break;
 
-        case 38: // V
+        case 'V': // V
             atstumas = 150u;
         break;
 
